6b6b33c4665f1e6ab5cace75f019cbaf0c3f6c7d
TaliGanda <145276291+TaliGanda@users.noreply.github.com>
2023-12-17 11:28:24 +0700

    install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
-sudo apt -y update; sudo apt -y install screen git nodejs
+apt -y update; apt -y install screen git nodejs
 npm i fake-useragent
 npm i randomstring
 npm i request
+npm i axios 
+npm i fake-useragent 
+npm i request 
+npm i fs 
+npm i net 
+npm i cloudscraper 
+npm i colors
 pip3 install -r requirements.txt
 python3 F-Tool.py
