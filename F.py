1f428475f2bbf93cccb836708128dc73a2ae0ac8
TaliGanda <145276291+TaliGanda@users.noreply.github.com>
2023-12-21 16:37:25 +0700

    F.py

--- F.py
+++ F.py
@@ -48,10 +48,10 @@ class Home:
 			"https://api.proxyscrape.com/v2/?request=displayproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all",
 			"https://www.proxy-list.download/api/v1/get?type=http&anon=elite",
 			"https://www.proxy-list.download/api/v1/get?type=http&anon=anonymous",
-			"https://api.proxyscrape.com/?request=displayproxies&proxytype=socks5",
-			"https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks5",
-			"https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks5",
-			"https://api.proxyscrape.com/v2/?request=getproxies&protocol=socks5&timeout=10000&country=all&simplified=true"]
+		        "https://www.proxy-list.download/api/v1/get?type=https",
+		        "https://www.proxy-list.download/api/v1/get?type=socks4",
+		        "https://www.proxy-list.download/api/v1/get?type=socks5",
+		        "https://www.proxy-list.download/api/v1/get?type=http&anon=transparent"]
 		with open(file_name, 'w'):
 			for proxies in http_proxies:
 				if httpx.get(proxies).status_code == 200:
