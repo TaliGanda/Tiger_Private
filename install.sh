13131ba44e348f8704a6800fc4300d9d05baab2d
TaliGanda <145276291+TaliGanda@users.noreply.github.com>
2023-12-22 12:38:13 +0700

    install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,29 @@
 apt -y update; apt -y install screen git nodejs
-sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates;curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -;sudo apt -y install nodejs;sudo apt -y install gcc g++ make;sudo apt -y install htop vnstat;sudo apt -y install screen;sudo apt -y install unrar;npm i events; npm i request; npm i os; npm i fs; npm i cluster; npm i cloudscraper; npm i url; npm i path; npm i net; npm i chalk;npm i sync-request;npm i random-useragent;npm i randomstring; npm i colors;npm i zombie;npm i random-useragent;sudo apt-get install unzip;echo
+sudo apt -y install curl dirmngr
+apt-transport-https lsb-release ca-certificates
+curl -sL https://deb.nodesource.com/setup_12.x 
+sudo -E bash -;sudo apt -y install nodejs
+sudo apt -y install gcc g++ make
+sudo apt -y install htop vnstat
+sudo apt -y install screen;sudo apt -y install unrar
+npm i events
+npm i request
+npm i os
+npm i fs
+npm i cluster
+npm i cloudscraper
+npm i url
+npm i path
+npm i net
+npm i chalk
+npm i sync-request
+npm i random-useragent
+npm i randomstring
+npm i colors
+npm i zombie
+npm i random-useragent
+sudo apt-get install
+unzip;echo
 npm i path
 npm i events
 npm i fake-useragent
